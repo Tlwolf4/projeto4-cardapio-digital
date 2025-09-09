dbc9cd48fec545429eaf285c1d7ec1126674fa9d
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2025-09-09 13:05:46 +0000

    criado script e index

---
+++ script.js
@@ -0,0 +1,12 @@
+// CARDAPIO
+let hamburguer = 1
+let pizza = 2
+let refrigerante = 3
+let fritas = 4
+
+alert(`Menu:
+   Hanbúrguer- R$ 20 
+   Pizza- R$ 35  
+   refrigerante- R$ 5
+   batatas fritas - R$ 12 `)
+let  escolha = parseInt(prompt("Escolha até 3 pratos ! "))
