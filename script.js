0033d5ec02a4e4b367a5a977ca98bedbb7e0cbd8
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2025-09-16 13:44:14 +0000

    alterado script e index

--- script.js
+++ script.js
@@ -17,8 +17,7 @@ let nome = prompt("Digite seu nome para regitro")
 
 
 let estudante = prompt(`Voce é estudante? (sim/não)`)
-if(estudante == "sim")
-    alert("Desconto de 10%")
+    
 let  prato1 = parseInt(prompt(`Menu:
    Hanbúrguer- R$ 20
    Pizza- R$ 35  
@@ -46,67 +45,102 @@ let  prato2 = parseInt(prompt(`Menu:
 let precoItem1 = 0
 switch(prato1) {
 case 1:
+    itemnome1 = "Hamburguer"
     precoitem1 = 20
     alert("Hamburguer- R$ 20  ")
     break
 case 2:
+    itemnome1 = "Pizza"
     precoItem1 = 35
     alert("Pizza- R$ 35")
     break
 case 3 :
-    precoItem1 = 5
+    itemnome1 = "Refrigerante"
+    preçoitem1 = 5
     alert("Refrigrante- R$ 5")
     break
 case 4 :
+    itemnome1 = "Batatas fritas"
     precoItem1 = 12
     alert("Batatas Fritas- R$ 12")
 }
 let precoItem2 = 0
 switch(prato2) {
 case 1: 
+itemnome2 = "Hambuguer"
     precoItem2 = 20
     alert("Hamburguer- R$ 20  ")
     break
 case 2:
+    itemnome2 = "Pizza"
     precoItem2 = 35
     alert("Pizza- R$ 35")
     break
 case 3 :
+    itemnome2 = "Refrigerante"
     precoItem2 = 5 
     alert("Refrigrante- R$ 5")
     break
 case 4 :
+    itemnome2 = "Batatas fritas"
     precoItem2 = 12
     alert("Batatas Fritas- R$ 12")
 }
 let precoItem3 = 0
 switch(prato3) {
 case 1:
+    itemnome3 = "Hamburguer"
     precoItem3 = 20 
     alert("Hamburguer- R$ 20  ")
     break
 case 2:
+     itemnome3 = "Pizza"
     precoItem3 = 35
     alert("Pizza- R$ 35")
     break
 case 3 :
+     itemnome3 = "Refrigerante"
     precoItem3 = 5
-    alert("Refrigrante- R$ 5")
+    alert("Refrigerante- R$ 5")
     break
-case 4 :
-    precoItem3 = 12
+    case 4 :
+    itemnome3 = "Batatas fritas"
+    precoItem2 = 12
     alert("Batatas Fritas- R$ 12")
-    break
 } 
  
 let total = precoItem1 + precoItem2 + precoItem3
 
-alert(`Tudo deu ${total}`)
-
-if(estudante == sim) { 
-
-} else(estudante == não)
-
-  let desconto = (`${total} * 0.2 `)
-  
-alert(`Com desconto fica ${desconto}`) 
+if (estudante == "sim" && total >= 50) {
+alert(`Obrigado ${nome}
+Pratos escolhidos:
+${itemnome1} de ${precoItem1}
+${itemnome2} de ${precoItem2}
+${itemnome3} de ${precoItem3}
+Total sem desconto: ${total}
+Total com desconto: ${total - (total * 0.15)}`)
+} else if (estudante == "sim" && total <= 50) {
+    alert(`Obrigado ${nome}
+Pratos escolhidos:
+${itemnome1} de ${precoItem1}
+${itemnome2} de ${precoItem2}
+${itemnome3} de ${precoItem3}
+Total sem desconto: ${total}
+Total com desconto: ${total - (total * 0.10)}`)
+} else if (estudante == "não" && total >= 50) {
+    alert(`Obrigado ${nome}
+Pratos escolhidos:
+${itemnome1} de ${precoItem1}
+${itemnome2} de ${precoItem2}
+${itemnome3} de ${precoItem3}
+Total sem desconto: ${total}
+Total com desconto: ${total - (total * 0.05)}`)
+} else {
+    alert(`Obrigado ${nome}
+Pratos escolhidos:
+${itemnome1} de ${precoItem1}
+${itemnome2} de ${precoItem2}
+${itemnome3} de ${precoItem3}
+Total sem desconto: ${total}
+Total com desconto: ${total}`)
+}
